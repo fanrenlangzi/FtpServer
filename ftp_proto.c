c91d883f2a9aebc3b3bf4f43a8b0a080a443a79b
guochy2012 <guochy2012@gmail.com>
2014-08-29 17:02:25 +0800

    增加FTP的状态码，封装ftp_reply函数

--- ftp_proto.c
+++ ftp_proto.c
@@ -2,12 +2,14 @@
 #include "common.h"
 #include "sysutil.h"
 #include "strutil.h"
+#include "ftp_codes.h"
+
+void ftp_reply(session_t *sess, int status, const char *text);
 
 //子进程不断的从FTP客户端接收FTP指令，并给与回应
 void handle_proto(session_t *sess)
 {
-	
-	writen(sess->peerfd, "220 (FtpServer 1.0)\r\n", strlen("220 (FtpServer 1.0)\r\n"));
+	ftp_reply(sess, FTP_GREET, "(FtpServer 1.0)");
 	while(1)
 	{
 		int ret = readline(sess->peerfd, sess->command, MAX_COMMAND);
@@ -25,4 +27,11 @@ void handle_proto(session_t *sess)
 		str_split(sess->command, sess->comm, sess->args, ' ');
 		printf("COMMD=[%s], ARGS=[%s]\n", sess->comm, sess->args);
 	}
+}
+
+void ftp_reply(session_t *sess, int status, const char *text)
+{
+	char tmp[1024] = { 0 };
+	snprintf(tmp, sizeof tmp, "%d %s\r\n", status, text);
+	writen(sess->peerfd, tmp, strlen(tmp));
 }
