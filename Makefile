5b7dcc71637ce77f87f4f269343593dcd2fe413b
guochy2012 <guochy2012@gmail.com>
2014-08-29 16:25:33 +0800

    使用配置文件

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@
 CC=gcc
 CFLAGS=-Wall -g
 BIN=FtpServer
-OBJS=main.o sysutil.o session.o strutil.o ftp_nobody.o ftp_proto.o
+OBJS=main.o sysutil.o session.o strutil.o ftp_nobody.o ftp_proto.o configure.o parse_conf.o
 $(BIN):$(OBJS)
 	$(CC) $(CFLAGS) $^ -o $@
 %.o:%.c
--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include "common.h"
 #include "sysutil.h"
 #include "session.h"
+#include "configure.h"
+#include "parse_conf.h"
+
+void print_conf();
 
 int main(int argc, char const *argv[])
 {
@@ -10,15 +14,18 @@ int main(int argc, char const *argv[])
 		exit(EXIT_FAILURE);
 	}
 
+	parseconf_load_file("ftpserver.conf");
+	print_conf();
+
 	//创建一个监听fd
-	int listenfd = tcp_server(NULL, 9981);
+	int listenfd = tcp_server(tunable_listen_address, tunable_listen_port);
 
 	pid_t pid;
 	session_t sess;
 	session_init(&sess);
 	while(1)
 	{
-		int peerfd = accept_timeout(listenfd, NULL, 10);
+		int peerfd = accept_timeout(listenfd, NULL, tunable_accept_timeout);
 		if(peerfd  == -1 && errno == ETIMEDOUT)
 			continue;
 		else if(peerfd == -1)
@@ -40,4 +47,26 @@ int main(int argc, char const *argv[])
 	}
 
 	return 0;
+}
+
+void print_conf()
+{
+	printf("tunable_pasv_enable=%d\n", tunable_pasv_enable);
+    printf("tunable_port_enable=%d\n", tunable_port_enable);
+
+    printf("tunable_listen_port=%u\n", tunable_listen_port);
+    printf("tunable_max_clients=%u\n", tunable_max_clients);
+    printf("tunable_max_per_ip=%u\n", tunable_max_per_ip);
+    printf("tunable_accept_timeout=%u\n", tunable_accept_timeout);
+    printf("tunable_connect_timeout=%u\n", tunable_connect_timeout);
+    printf("tunable_idle_session_timeout=%u\n", tunable_idle_session_timeout);
+    printf("tunable_data_connection_timeout=%u\n", tunable_data_connection_timeout);
+    printf("tunable_local_umask=0%o\n", tunable_local_umask);
+    printf("tunable_upload_max_rate=%u\n", tunable_upload_max_rate);
+    printf("tunable_download_max_rate=%u\n", tunable_download_max_rate);
+
+    if (tunable_listen_address == NULL)
+        printf("tunable_listen_address=NULL\n");
+    else
+        printf("tunable_listen_address=%s\n", tunable_listen_address); 
 }
