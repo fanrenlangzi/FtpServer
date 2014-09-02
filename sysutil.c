adfbb1e03d7994529821713436c630f2eb324594
guochy2012 <guochy2012@gmail.com>
2014-09-02 17:36:11 +0800

    fix code

--- sysutil.c
+++ sysutil.c
@@ -10,8 +10,8 @@ int tcp_client(unsigned int port)
 	if(port > 0)
 	{
 		int on = 1;
-        if((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char *)&on, sizeof(on))) == -1)
-            ERR_EXIT("setsockopt");
+		if((setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, (const char *)&on, sizeof(on))) == -1)
+			ERR_EXIT("setsockopt");
 
 		struct sockaddr_in addr;
 		memset(&addr, 0, sizeof addr);
@@ -34,101 +34,101 @@ int tcp_client(unsigned int port)
  * @port: 服务器端口
  * 成功返回监听套接字
  */
-int tcp_server(const char *host, unsigned short port)
-{
-	int listenfd;
-	if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
-		ERR_EXIT("tcp_server");
+ int tcp_server(const char *host, unsigned short port)
+ {
+ 	int listenfd;
+ 	if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+ 		ERR_EXIT("tcp_server");
 
-	struct sockaddr_in servaddr;
-	memset(&servaddr, 0, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	if (host != NULL)
-	{
-		if (inet_aton(host, &servaddr.sin_addr) == 0)
-		{
-			struct hostent *hp;
-			hp = gethostbyname(host);
-			if (hp == NULL)
-				ERR_EXIT("gethostbyname");
+ 	struct sockaddr_in servaddr;
+ 	memset(&servaddr, 0, sizeof(servaddr));
+ 	servaddr.sin_family = AF_INET;
+ 	if (host != NULL)
+ 	{
+ 		if (inet_aton(host, &servaddr.sin_addr) == 0)
+ 		{
+ 			struct hostent *hp;
+ 			hp = gethostbyname(host);
+ 			if (hp == NULL)
+ 				ERR_EXIT("gethostbyname");
 
-			servaddr.sin_addr = *(struct in_addr*)hp->h_addr;
-		}
-	}
-	else
-		servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+ 			servaddr.sin_addr = *(struct in_addr*)hp->h_addr;
+ 		}
+ 	}
+ 	else
+ 		servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	servaddr.sin_port = htons(port);
+ 	servaddr.sin_port = htons(port);
 
-	int on = 1;
-	if ((setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof(on))) < 0)
-		ERR_EXIT("gethostbyname");
+ 	int on = 1;
+ 	if ((setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const char*)&on, sizeof(on))) < 0)
+ 		ERR_EXIT("gethostbyname");
 
-	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
-		ERR_EXIT("bind");
+ 	if (bind(listenfd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0)
+ 		ERR_EXIT("bind");
 
-	if (listen(listenfd, SOMAXCONN) < 0)
-		ERR_EXIT("listen");
+ 	if (listen(listenfd, SOMAXCONN) < 0)
+ 		ERR_EXIT("listen");
 
-	return listenfd;
-}
+ 	return listenfd;
+ }
 
-int get_local_ip(char *ip)
-{
-	int sockfd; 
-    if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
-    {
-        ERR_EXIT("socket");
-    }
+ int get_local_ip(char *ip)
+ {
+ 	int sockfd; 
+ 	if((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1)
+ 	{
+ 		ERR_EXIT("socket");
+ 	}
 
-    struct ifreq req;
-    
-    bzero(&req, sizeof(struct ifreq));
-    strcpy(req.ifr_name, "eth0");
+ 	struct ifreq req;
+ 	
+ 	bzero(&req, sizeof(struct ifreq));
+ 	strcpy(req.ifr_name, "eth0");
 
-    if(ioctl(sockfd, SIOCGIFADDR, &req) == -1)
-    	ERR_EXIT("ioctl");
+ 	if(ioctl(sockfd, SIOCGIFADDR, &req) == -1)
+ 		ERR_EXIT("ioctl");
 
-	struct sockaddr_in *host = (struct sockaddr_in*)&req.ifr_addr;
-    strcpy(ip, inet_ntoa(host->sin_addr));
-    close(sockfd);
-    return 1;
-}
+ 	struct sockaddr_in *host = (struct sockaddr_in*)&req.ifr_addr;
+ 	strcpy(ip, inet_ntoa(host->sin_addr));
+ 	close(sockfd);
+ 	return 1;
+ }
 
 
 /**
  * activate_noblock - 设置I/O为非阻塞模式
  * @fd: 文件描符符
  */
-void activate_nonblock(int fd)
-{
-	int ret;
-	int flags = fcntl(fd, F_GETFL);
-	if (flags == -1)
-		ERR_EXIT("fcntl");
+ void activate_nonblock(int fd)
+ {
+ 	int ret;
+ 	int flags = fcntl(fd, F_GETFL);
+ 	if (flags == -1)
+ 		ERR_EXIT("fcntl");
 
-	flags |= O_NONBLOCK;
-	ret = fcntl(fd, F_SETFL, flags);
-	if (ret == -1)
-		ERR_EXIT("fcntl");
-}
+ 	flags |= O_NONBLOCK;
+ 	ret = fcntl(fd, F_SETFL, flags);
+ 	if (ret == -1)
+ 		ERR_EXIT("fcntl");
+ }
 
 /**
  * deactivate_nonblock - 设置I/O为阻塞模式
  * @fd: 文件描符符
  */
-void deactivate_nonblock(int fd)
-{
-	int ret;
-	int flags = fcntl(fd, F_GETFL);
-	if (flags == -1)
-		ERR_EXIT("fcntl");
+ void deactivate_nonblock(int fd)
+ {
+ 	int ret;
+ 	int flags = fcntl(fd, F_GETFL);
+ 	if (flags == -1)
+ 		ERR_EXIT("fcntl");
 
-	flags &= ~O_NONBLOCK;
-	ret = fcntl(fd, F_SETFL, flags);
-	if (ret == -1)
-		ERR_EXIT("fcntl");
-}
+ 	flags &= ~O_NONBLOCK;
+ 	ret = fcntl(fd, F_SETFL, flags);
+ 	if (ret == -1)
+ 		ERR_EXIT("fcntl");
+ }
 
 
 /**
@@ -137,35 +137,35 @@ void deactivate_nonblock(int fd)
  * @wait_seconds: 等待超时秒数，如果为0表示不检测超时
  * 成功（未超时）返回0，失败返回-1，超时返回-1并且errno = ETIMEDOUT
  */
-int read_timeout(int fd, unsigned int wait_seconds)
-{
-	int ret = 0;
-	if (wait_seconds > 0)
-	{
-		fd_set read_fdset;
-		struct timeval timeout;
+ int read_timeout(int fd, unsigned int wait_seconds)
+ {
+ 	int ret = 0;
+ 	if (wait_seconds > 0)
+ 	{
+ 		fd_set read_fdset;
+ 		struct timeval timeout;
 
-		FD_ZERO(&read_fdset);
-		FD_SET(fd, &read_fdset);
+ 		FD_ZERO(&read_fdset);
+ 		FD_SET(fd, &read_fdset);
 
-		timeout.tv_sec = wait_seconds;
-		timeout.tv_usec = 0;
-		do
-		{
-			ret = select(fd + 1, &read_fdset, NULL, NULL, &timeout);
-		} while (ret < 0 && errno == EINTR);
+ 		timeout.tv_sec = wait_seconds;
+ 		timeout.tv_usec = 0;
+ 		do
+ 		{
+ 			ret = select(fd + 1, &read_fdset, NULL, NULL, &timeout);
+ 		} while (ret < 0 && errno == EINTR);
 
-		if (ret == 0)
-		{
-			ret = -1;
-			errno = ETIMEDOUT;
-		}
-		else if (ret == 1)
-			ret = 0;
-	}
+ 		if (ret == 0)
+ 		{
+ 			ret = -1;
+ 			errno = ETIMEDOUT;
+ 		}
+ 		else if (ret == 1)
+ 			ret = 0;
+ 	}
 
-	return ret;
-}
+ 	return ret;
+ }
 
 /**
  * write_timeout - 读超时检测函数，不含写操作
@@ -173,35 +173,35 @@ int read_timeout(int fd, unsigned int wait_seconds)
  * @wait_seconds: 等待超时秒数，如果为0表示不检测超时
  * 成功（未超时）返回0，失败返回-1，超时返回-1并且errno = ETIMEDOUT
  */
-int write_timeout(int fd, unsigned int wait_seconds)
-{
-	int ret = 0;
-	if (wait_seconds > 0)
-	{
-		fd_set write_fdset;
-		struct timeval timeout;
+ int write_timeout(int fd, unsigned int wait_seconds)
+ {
+ 	int ret = 0;
+ 	if (wait_seconds > 0)
+ 	{
+ 		fd_set write_fdset;
+ 		struct timeval timeout;
 
-		FD_ZERO(&write_fdset);
-		FD_SET(fd, &write_fdset);
+ 		FD_ZERO(&write_fdset);
+ 		FD_SET(fd, &write_fdset);
 
-		timeout.tv_sec = wait_seconds;
-		timeout.tv_usec = 0;
-		do
-		{
-			ret = select(fd + 1, NULL, &write_fdset, NULL, &timeout);
-		} while (ret < 0 && errno == EINTR);
+ 		timeout.tv_sec = wait_seconds;
+ 		timeout.tv_usec = 0;
+ 		do
+ 		{
+ 			ret = select(fd + 1, NULL, &write_fdset, NULL, &timeout);
+ 		} while (ret < 0 && errno == EINTR);
 
-		if (ret == 0)
-		{
-			ret = -1;
-			errno = ETIMEDOUT;
-		}
-		else if (ret == 1)
-			ret = 0;
-	}
+ 		if (ret == 0)
+ 		{
+ 			ret = -1;
+ 			errno = ETIMEDOUT;
+ 		}
+ 		else if (ret == 1)
+ 			ret = 0;
+ 	}
 
-	return ret;
-}
+ 	return ret;
+ }
 
 /**
  * accept_timeout - 带超时的accept
@@ -210,42 +210,42 @@ int write_timeout(int fd, unsigned int wait_seconds)
  * @wait_seconds: 等待超时秒数，如果为0表示正常模式
  * 成功（未超时）返回已连接套接字，超时返回-1并且errno = ETIMEDOUT
  */
-int accept_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
-{
-	int ret;
-	socklen_t addrlen = sizeof(struct sockaddr_in);
+ int accept_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
+ {
+ 	int ret;
+ 	socklen_t addrlen = sizeof(struct sockaddr_in);
 
-	if (wait_seconds > 0)
-	{
-		fd_set accept_fdset;
-		struct timeval timeout;
-		FD_ZERO(&accept_fdset);
-		FD_SET(fd, &accept_fdset);
-		timeout.tv_sec = wait_seconds;
-		timeout.tv_usec = 0;
-		do
-		{
-			ret = select(fd + 1, &accept_fdset, NULL, NULL, &timeout);
-		} while (ret < 0 && errno == EINTR);
-		if (ret == -1)
-			return -1;
-		else if (ret == 0)
-		{
-			errno = ETIMEDOUT;
-			return -1;
-		}
-	}
+ 	if (wait_seconds > 0)
+ 	{
+ 		fd_set accept_fdset;
+ 		struct timeval timeout;
+ 		FD_ZERO(&accept_fdset);
+ 		FD_SET(fd, &accept_fdset);
+ 		timeout.tv_sec = wait_seconds;
+ 		timeout.tv_usec = 0;
+ 		do
+ 		{
+ 			ret = select(fd + 1, &accept_fdset, NULL, NULL, &timeout);
+ 		} while (ret < 0 && errno == EINTR);
+ 		if (ret == -1)
+ 			return -1;
+ 		else if (ret == 0)
+ 		{
+ 			errno = ETIMEDOUT;
+ 			return -1;
+ 		}
+ 	}
 
-	if (addr != NULL)
-		ret = accept(fd, (struct sockaddr*)addr, &addrlen);
-	else
-		ret = accept(fd, NULL, NULL);
+ 	if (addr != NULL)
+ 		ret = accept(fd, (struct sockaddr*)addr, &addrlen);
+ 	else
+ 		ret = accept(fd, NULL, NULL);
 /*	if (ret == -1)
 		ERR_EXIT("accept");
 		*/
 
-	return ret;
-}
+		return ret;
+	}
 
 /**
  * connect_timeout - connect
@@ -254,63 +254,63 @@ int accept_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
  * @wait_seconds: 等待超时秒数，如果为0表示正常模式
  * 成功（未超时）返回0，失败返回-1，超时返回-1并且errno = ETIMEDOUT
  */
-int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
-{
-	int ret;
-	socklen_t addrlen = sizeof(struct sockaddr_in);
+ int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
+ {
+ 	int ret;
+ 	socklen_t addrlen = sizeof(struct sockaddr_in);
 
-	if (wait_seconds > 0)
-		activate_nonblock(fd);
+ 	if (wait_seconds > 0)
+ 		activate_nonblock(fd);
 
-	ret = connect(fd, (struct sockaddr*)addr, addrlen);
-	if (ret < 0 && errno == EINPROGRESS)
-	{
-		fd_set connect_fdset;
-		struct timeval timeout;
-		FD_ZERO(&connect_fdset);
-		FD_SET(fd, &connect_fdset);
-		timeout.tv_sec = wait_seconds;
-		timeout.tv_usec = 0;
-		do
-		{
+ 	ret = connect(fd, (struct sockaddr*)addr, addrlen);
+ 	if (ret < 0 && errno == EINPROGRESS)
+ 	{
+ 		fd_set connect_fdset;
+ 		struct timeval timeout;
+ 		FD_ZERO(&connect_fdset);
+ 		FD_SET(fd, &connect_fdset);
+ 		timeout.tv_sec = wait_seconds;
+ 		timeout.tv_usec = 0;
+ 		do
+ 		{
 			/* 一量连接建立，套接字就可写 */
-			ret = select(fd + 1, NULL, &connect_fdset, NULL, &timeout);
-		} while (ret < 0 && errno == EINTR);
-		if (ret == 0)
-		{
-			ret = -1;
-			errno = ETIMEDOUT;
-		}
-		else if (ret < 0)
-			return -1;
-		else if (ret == 1)
-		{
+ 			ret = select(fd + 1, NULL, &connect_fdset, NULL, &timeout);
+ 		} while (ret < 0 && errno == EINTR);
+ 		if (ret == 0)
+ 		{
+ 			ret = -1;
+ 			errno = ETIMEDOUT;
+ 		}
+ 		else if (ret < 0)
+ 			return -1;
+ 		else if (ret == 1)
+ 		{
 			/* ret返回为1，可能有两种情况，一种是连接建立成功，一种是套接字产生错误，*/
 			/* 此时错误信息不会保存至errno变量中，因此，需要调用getsockopt来获取。 */
-			int err;
-			socklen_t socklen = sizeof(err);
-			int sockoptret = getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &socklen);
-			if (sockoptret == -1)
-			{
-				return -1;
-			}
-			if (err == 0)
-			{
-				ret = 0;
-			}
-			else
-			{
-				errno = err;
-				ret = -1;
-			}
-		}
-	}
-	if (wait_seconds > 0)
-	{
-		deactivate_nonblock(fd);
-	}
-	return ret;
-}
+ 			int err;
+ 			socklen_t socklen = sizeof(err);
+ 			int sockoptret = getsockopt(fd, SOL_SOCKET, SO_ERROR, &err, &socklen);
+ 			if (sockoptret == -1)
+ 			{
+ 				return -1;
+ 			}
+ 			if (err == 0)
+ 			{
+ 				ret = 0;
+ 			}
+ 			else
+ 			{
+ 				errno = err;
+ 				ret = -1;
+ 			}
+ 		}
+ 	}
+ 	if (wait_seconds > 0)
+ 	{
+ 		deactivate_nonblock(fd);
+ 	}
+ 	return ret;
+ }
 
 /**
  * readn - 读取固定字节数
@@ -319,29 +319,29 @@ int connect_timeout(int fd, struct sockaddr_in *addr, unsigned int wait_seconds)
  * @count: 要读取的字节数
  * 成功返回count，失败返回-1，读到EOF返回<count
  */
-ssize_t readn(int fd, void *buf, size_t count)
-{
-	size_t nleft = count;
-	ssize_t nread;
-	char *bufp = (char*)buf;
+ ssize_t readn(int fd, void *buf, size_t count)
+ {
+ 	size_t nleft = count;
+ 	ssize_t nread;
+ 	char *bufp = (char*)buf;
 
-	while (nleft > 0)
-	{
-		if ((nread = read(fd, bufp, nleft)) < 0)
-		{
-			if (errno == EINTR)
-				continue;
-			return -1;
-		}
-		else if (nread == 0)
-			return count - nleft;
+ 	while (nleft > 0)
+ 	{
+ 		if ((nread = read(fd, bufp, nleft)) < 0)
+ 		{
+ 			if (errno == EINTR)
+ 				continue;
+ 			return -1;
+ 		}
+ 		else if (nread == 0)
+ 			return count - nleft;
 
-		bufp += nread;
-		nleft -= nread;
-	}
+ 		bufp += nread;
+ 		nleft -= nread;
+ 	}
 
-	return count;
-}
+ 	return count;
+ }
 
 /**
  * writen - 发送固定字节数
@@ -350,29 +350,29 @@ ssize_t readn(int fd, void *buf, size_t count)
  * @count: 要读取的字节数
  * 成功返回count，失败返回-1
  */
-ssize_t writen(int fd, const void *buf, size_t count)
-{
-	size_t nleft = count;
-	ssize_t nwritten;
-	char *bufp = (char*)buf;
+ ssize_t writen(int fd, const void *buf, size_t count)
+ {
+ 	size_t nleft = count;
+ 	ssize_t nwritten;
+ 	char *bufp = (char*)buf;
 
-	while (nleft > 0)
-	{
-		if ((nwritten = write(fd, bufp, nleft)) < 0)
-		{
-			if (errno == EINTR)
-				continue;
-			return -1;
-		}
-		else if (nwritten == 0)
-			continue;
+ 	while (nleft > 0)
+ 	{
+ 		if ((nwritten = write(fd, bufp, nleft)) < 0)
+ 		{
+ 			if (errno == EINTR)
+ 				continue;
+ 			return -1;
+ 		}
+ 		else if (nwritten == 0)
+ 			continue;
 
-		bufp += nwritten;
-		nleft -= nwritten;
-	}
+ 		bufp += nwritten;
+ 		nleft -= nwritten;
+ 	}
 
-	return count;
-}
+ 	return count;
+ }
 
 /**
  * recv_peek - 仅仅查看套接字缓冲区数据，但不移除数据
@@ -381,16 +381,16 @@ ssize_t writen(int fd, const void *buf, size_t count)
  * @len: 长度
  * 成功返回>=0，失败返回-1
  */
-ssize_t recv_peek(int sockfd, void *buf, size_t len) {
-    int nread;
-    while (1) {
+ ssize_t recv_peek(int sockfd, void *buf, size_t len) {
+ 	int nread;
+ 	while (1) {
         //这个过程只成功调用一次
-        nread = recv(sockfd, buf, len, MSG_PEEK);
+ 		nread = recv(sockfd, buf, len, MSG_PEEK);
         if (nread < 0 && errno == EINTR) {  //被中断则继续读取
-            continue;
+        	continue;
         }
         if (nread < 0) {
-            return -1;
+        	return -1;
         }
         break;
     }
@@ -404,7 +404,7 @@ ssize_t recv_peek(int sockfd, void *buf, size_t len) {
  * @maxline: 每行最大长度
  * 成功返回>=0，失败返回-1
  */
-ssize_t readline(int sockfd, void *buf, size_t maxline) {
+ ssize_t readline(int sockfd, void *buf, size_t maxline) {
     int nread;  //一次IO读取的数量
     int nleft;  //还剩余的字节数
     char *ptr;  //存放数据的指针的位置
@@ -417,31 +417,31 @@ ssize_t readline(int sockfd, void *buf, size_t maxline) {
     while (nleft > 0) {
         //这一次调用仅仅是预览数据
         //并没有真的把数据从缓冲区中取走
-        ret = recv_peek(sockfd, ptr, nleft);
+    	ret = recv_peek(sockfd, ptr, nleft);
         //注意这里读取的字节不够，绝对不是错误！！！
-        if (ret <= 0) {
-            return ret;
-        }
+    	if (ret <= 0) {
+    		return ret;
+    	}
 
-        nread = ret;
-        int i;
-        for (i = 0; i < nread; ++i) {
-            if (ptr[i] == '\n') {
+    	nread = ret;
+    	int i;
+    	for (i = 0; i < nread; ++i) {
+    		if (ptr[i] == '\n') {
                 //这里才是真正的读取过程
-                ret = readn(sockfd, ptr, i + 1);
-                if (ret != i + 1) {
-                    return -1;
-                }
-                total += ret;
-                ptr += ret;
-                *ptr = 0;
+    			ret = readn(sockfd, ptr, i + 1);
+    			if (ret != i + 1) {
+    				return -1;
+    			}
+    			total += ret;
+    			ptr += ret;
+    			*ptr = 0;
                 return total;   //返回此行的长度 '\n'包含在其中
             }
         }
         //如果没有发现\n，这些数据应全部接收
         ret = readn(sockfd, ptr, nread);
         if (ret != nread) {
-            return -1;
+        	return -1;
         }
         nleft -= nread;
         total += nread;
